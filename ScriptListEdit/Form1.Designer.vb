0f6e921ed765f56e42585bdbcf15f937e23cebf1
loosepolygon <loosepolygon@gmail.com>
2017-06-02 00:11:47 -0500

    Initial commit, just a blank class and window.

---
+++ ScriptListEdit/Form1.Designer.vb
@@ -0,0 +1,39 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class frmScriptListEdit
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SuspendLayout()
+        '
+        'frmScriptListEdit
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(673, 635)
+        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
+        Me.MinimumSize = New System.Drawing.Size(200, 200)
+        Me.Name = "frmScriptListEdit"
+        Me.Text = "Script List Editor"
+        Me.ResumeLayout(False)
+
+    End Sub
+End Class
---
+++ ScriptListEdit/Form1.vb
@@ -0,0 +1,5 @@
+﻿Imports System.IO
+
+Public Class frmScriptListEdit
+
+End Class
