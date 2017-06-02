8ec129e64ea753c07982418d5b55f4fa62415046
loosepolygon <loosepolygon@gmail.com>
2017-06-02 02:05:59 -0500

    Added luainfo loading.

--- ScriptListEdit/Form1.Designer.vb
+++ ScriptListEdit/Form1.Designer.vb
@@ -22,18 +22,121 @@ Partial Class frmScriptListEdit
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
+        Me.btnSave = New System.Windows.Forms.Button()
+        Me.btnOpen = New System.Windows.Forms.Button()
+        Me.txtFile = New System.Windows.Forms.TextBox()
+        Me.lblGAFile = New System.Windows.Forms.Label()
+        Me.tabControlRoot = New System.Windows.Forms.TabControl()
+        Me.tabLuainfo = New System.Windows.Forms.TabPage()
+        Me.dgvLuainfo = New System.Windows.Forms.DataGridView()
+        Me.tabControlRoot.SuspendLayout()
+        Me.tabLuainfo.SuspendLayout()
+        CType(Me.dgvLuainfo, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
+        '
+        'btnSave
+        '
+        Me.btnSave.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnSave.Location = New System.Drawing.Point(743, 40)
+        Me.btnSave.Name = "btnSave"
+        Me.btnSave.Size = New System.Drawing.Size(75, 23)
+        Me.btnSave.TabIndex = 35
+        Me.btnSave.Text = "Save"
+        Me.btnSave.UseVisualStyleBackColor = True
+        '
+        'btnOpen
+        '
+        Me.btnOpen.Anchor = CType((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.btnOpen.Location = New System.Drawing.Point(665, 40)
+        Me.btnOpen.Name = "btnOpen"
+        Me.btnOpen.Size = New System.Drawing.Size(75, 23)
+        Me.btnOpen.TabIndex = 34
+        Me.btnOpen.Text = "Open"
+        Me.btnOpen.UseVisualStyleBackColor = True
+        '
+        'txtFile
+        '
+        Me.txtFile.AllowDrop = True
+        Me.txtFile.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.txtFile.Location = New System.Drawing.Point(46, 14)
+        Me.txtFile.Name = "txtFile"
+        Me.txtFile.Size = New System.Drawing.Size(772, 20)
+        Me.txtFile.TabIndex = 31
+        '
+        'lblGAFile
+        '
+        Me.lblGAFile.AutoSize = True
+        Me.lblGAFile.Location = New System.Drawing.Point(14, 17)
+        Me.lblGAFile.Name = "lblGAFile"
+        Me.lblGAFile.Size = New System.Drawing.Size(26, 13)
+        Me.lblGAFile.TabIndex = 33
+        Me.lblGAFile.Text = "File:"
+        '
+        'tabControlRoot
+        '
+        Me.tabControlRoot.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.tabControlRoot.Controls.Add(Me.tabLuainfo)
+        Me.tabControlRoot.Location = New System.Drawing.Point(12, 66)
+        Me.tabControlRoot.Margin = New System.Windows.Forms.Padding(0)
+        Me.tabControlRoot.Name = "tabControlRoot"
+        Me.tabControlRoot.Padding = New System.Drawing.Point(10, 4)
+        Me.tabControlRoot.SelectedIndex = 0
+        Me.tabControlRoot.Size = New System.Drawing.Size(806, 686)
+        Me.tabControlRoot.TabIndex = 46
+        '
+        'tabLuainfo
+        '
+        Me.tabLuainfo.Controls.Add(Me.dgvLuainfo)
+        Me.tabLuainfo.Location = New System.Drawing.Point(4, 24)
+        Me.tabLuainfo.Margin = New System.Windows.Forms.Padding(0)
+        Me.tabLuainfo.Name = "tabLuainfo"
+        Me.tabLuainfo.Padding = New System.Windows.Forms.Padding(3)
+        Me.tabLuainfo.Size = New System.Drawing.Size(798, 658)
+        Me.tabLuainfo.TabIndex = 2
+        Me.tabLuainfo.Text = "luainfo"
+        Me.tabLuainfo.UseVisualStyleBackColor = True
+        '
+        'dgvLuainfo
+        '
+        Me.dgvLuainfo.AllowUserToAddRows = False
+        Me.dgvLuainfo.AllowUserToDeleteRows = False
+        Me.dgvLuainfo.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.AllCells
+        Me.dgvLuainfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgvLuainfo.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.dgvLuainfo.Location = New System.Drawing.Point(3, 3)
+        Me.dgvLuainfo.Margin = New System.Windows.Forms.Padding(0)
+        Me.dgvLuainfo.Name = "dgvLuainfo"
+        Me.dgvLuainfo.RowHeadersWidth = 60
+        Me.dgvLuainfo.Size = New System.Drawing.Size(792, 652)
+        Me.dgvLuainfo.TabIndex = 39
         '
         'frmScriptListEdit
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(673, 635)
-        Me.Margin = New System.Windows.Forms.Padding(3, 4, 3, 4)
-        Me.MinimumSize = New System.Drawing.Size(200, 200)
+        Me.ClientSize = New System.Drawing.Size(834, 761)
+        Me.Controls.Add(Me.tabControlRoot)
+        Me.Controls.Add(Me.btnSave)
+        Me.Controls.Add(Me.btnOpen)
+        Me.Controls.Add(Me.txtFile)
+        Me.Controls.Add(Me.lblGAFile)
         Me.Name = "frmScriptListEdit"
-        Me.Text = "Script List Editor"
+        Me.Text = "Script List Edit"
+        Me.tabControlRoot.ResumeLayout(False)
+        Me.tabLuainfo.ResumeLayout(False)
+        CType(Me.dgvLuainfo, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
+        Me.PerformLayout()
 
     End Sub
+    Friend WithEvents btnSave As System.Windows.Forms.Button
+    Friend WithEvents btnOpen As System.Windows.Forms.Button
+    Friend WithEvents txtFile As System.Windows.Forms.TextBox
+    Friend WithEvents lblGAFile As System.Windows.Forms.Label
+    Friend WithEvents tabControlRoot As TabControl
+    Friend WithEvents tabLuainfo As TabPage
+    Friend WithEvents dgvLuainfo As DataGridView
 End Class
--- ScriptListEdit/Form1.vb
+++ ScriptListEdit/Form1.vb
@@ -1,5 +1,152 @@
 ﻿Imports System.IO
+Imports System.Text
 
 Public Class frmScriptListEdit
+    Private luainfoLayout As dgvLayout
 
+    Private headerUnk1 As Integer
+    Private headerUnk2 As Integer
+
+    Public Shared bytes() As Byte
+    Public Shared bigEndian As Boolean
+
+    Private Sub txt_Drop(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtFile.DragDrop
+        Dim file() As String = e.Data.GetData(DataFormats.FileDrop)
+        sender.Text = file(0)
+    End Sub
+    Private Sub txt_DragEnter(sender As Object, e As System.Windows.Forms.DragEventArgs) Handles txtFile.DragEnter
+        e.Effect = DragDropEffects.Copy
+    End Sub
+
+    Private Function RawStrFromBytes(ByVal loc As UInteger, Optional ByVal maxLength As Integer = Int32.MaxValue) As Byte()
+        Dim cont As Boolean = True
+        Dim len As Integer = 0
+
+        While cont
+            If bytes(loc + len) > 0 And len < maxLength Then
+                len += 1
+            Else
+                cont = False
+            End If
+        End While
+
+        If len = 0 Then
+            Return {}
+        End If
+
+        Dim strBytesJIS(len - 1) As Byte
+        Array.Copy(bytes, loc, strBytesJIS, 0, len)
+
+        Return strBytesJIS
+    End Function
+    Private Function RawStrToStr(ByVal rawStr As Byte()) As String
+        Dim enc1 = Encoding.GetEncoding("shift_jis")
+        Dim enc2 = Encoding.Unicode
+        Dim strBytesUnicode As Byte() = Encoding.Convert(enc1, enc2, rawStr)
+        Dim str As String = Encoding.Unicode.GetString(strBytesUnicode)
+        Return str
+    End Function
+
+    Private Function ReverseFourBytes(ByVal byt() As Byte)
+        Return {byt(3), byt(2), byt(1), byt(0)}
+    End Function
+
+    Private Function SIntFromFour(ByVal loc As UInteger) As Integer
+        Dim tmpint As Integer = 0
+        Dim bArray(3) As Byte
+
+        For i = 0 To 3
+            bArray(3 - i) = bytes(loc + i)
+        Next
+        If Not bigEndian Then bArray = ReverseFourBytes(bArray)
+        tmpint = BitConverter.ToInt32(bArray, 0)
+        Return tmpint
+    End Function
+
+    Private Sub initDGVs()
+        Dim dgvs() As DataGridView = {dgvLuainfo}
+        Dim layouts() As dgvLayout = {luainfoLayout}
+
+        For i = 0 To layouts.Count - 1
+            Dim dgv = dgvs(i)
+            Dim layout = layouts(i)
+
+            dgv.Rows.Clear()
+            dgv.Columns.Clear()
+
+            For j = 0 To luainfoLayout.fieldCount - 1
+                dgv.Columns.Add(layout.fieldNames(j), layout.fieldNames(j))
+                dgv.Columns(j).DefaultCellStyle.BackColor = layout.fieldBGColors(j)
+                dgv.Columns(j).DefaultCellStyle.ForeColor = Color.Black
+                dgv.Columns(j).SortMode = DataGridViewColumnSortMode.NotSortable
+            Next
+        Next
+    End Sub
+
+    Private Sub btnOpen_Click(sender As Object, e As EventArgs) Handles btnOpen.Click
+        bytes = File.ReadAllBytes(txtFile.Text)
+
+        bigEndian = False
+
+        initDGVs()
+
+        Dim rawStr As Byte() = RawStrFromBytes(&H0, 4)
+        Dim signature As String = RawStrToStr(rawStr)
+
+        If signature <> "LUAI" Then
+            Throw New ApplicationException("Not a luainfo file")
+        End If
+
+        headerUnk1 = SIntFromFour(&H4)
+
+        Dim scriptCount As Integer = SIntFromFour(&H8)
+
+        headerUnk2 = SIntFromFour(&HC)
+
+        For i = 0 To scriptCount - 1
+            Dim npcID = SIntFromFour(&H10 + &H10 * i + &H0)
+            Dim offset = SIntFromFour(&H10 + &H10 * i + &H4)
+            Dim unk1 = SIntFromFour(&H10 + &H10 * i + &H8)
+            Dim unk2 = SIntFromFour(&H10 + &H10 * i + &HC)
+            Dim scriptName = RawStrToStr(RawStrFromBytes(offset))
+
+            Dim partRow(luainfoLayout.fieldCount - 1) As String
+
+            partRow(luainfoLayout.getFieldIndex("Name")) = scriptName
+            partRow(luainfoLayout.getFieldIndex("NPC ID")) = npcID
+            partRow(luainfoLayout.getFieldIndex("Unk1")) = unk1
+            partRow(luainfoLayout.getFieldIndex("Unk2")) = unk2
+            dgvLuainfo.Rows.Add(partRow)
+        Next
+
+    End Sub
+
+    Private Sub frmScriptListEdit_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        luainfoLayout = New dgvLayout
+        luainfoLayout.add("Name", "string", Color.White)
+        luainfoLayout.add("NPC ID", "i32", Color.White)
+        luainfoLayout.add("Unk1", "i32", Color.LightGray)
+        luainfoLayout.add("Unk2", "i32", Color.LightGray)
+    End Sub
+
+End Class
+
+
+Public Class dgvLayout
+    Public fieldNames As List(Of String) = New List(Of String)
+    Public fieldtypes As List(Of String) = New List(Of String)
+    Public fieldBGColors As List(Of Color) = New List(Of Color)
+
+    Public Sub add(ByVal name As String, ByVal type As String, Col As Color)
+        fieldNames.Add(name)
+        fieldtypes.Add(type)
+        fieldBGColors.Add(Col)
+    End Sub
+
+    Public Function fieldCount() As Integer
+        Return fieldNames.Count
+    End Function
+    Public Function getFieldIndex(ByVal name As String) As Integer
+        Return fieldNames.IndexOf(name)
+    End Function
 End Class
